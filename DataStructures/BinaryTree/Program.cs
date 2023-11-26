bbd88c5ad9608a112064ad9d4bf4bc4f85da1a13
Max Zadarnovskiy <maxzadarnovskiyy@gmail.com>
2023-11-26 17:05:58 +1030

    Day 5 - Start learning Binary Trees

---
+++ DataStructures/BinaryTree/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Binary Tree");
+var rootNode = new TreeNode<int>(0);
+
+public class TreeNode<T>
+{
+    public T Value { get; set; }
+    public TreeNode<T>? Left { get; set; } = null;
+    public TreeNode<T>? Right { get; set; } = null;
+
+    public TreeNode(T data)
+    {
+        Value = data;
+    }
+
+    public void AddLeft(TreeNode<T> left) => Left = left;
+    public void AddRight(TreeNode<T> right) => Right = right;
+}
+
+public class BinaryTree<T>
+{
+    public TreeNode<T> RootNode { get; set; }
+    public BinaryTree(TreeNode<T> rootNode)
+    {
+        RootNode = rootNode;
+    }
+
+
+}
