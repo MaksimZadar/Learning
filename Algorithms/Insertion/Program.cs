e16a370e9ab0bbfb4ebf54b248ae5f888830d922
MaksimZadar <maxzadarnovskiyy@gmail.com>
2023-11-29 21:50:41 +1030

    Day 6 Kinda - Insertion Sort

---
+++ Algorithms/Insertion/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Insertion Algorithm");
+
+Console.WriteLine("Starting array: ");
+int Min = 0;
+int NumOfNumbers = 100000;
+
+int[] array = new int[NumOfNumbers]; 
+
+Random randNum = new();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = randNum.Next(Min, NumOfNumbers);
+}
+foreach (var num in array)
+{
+    Console.Write($"{num} ");
+}
+Console.WriteLine("Sorting...");
+var watch = new Stopwatch();
+watch.Start();
+for (int i = 1; i < array.Length; i++)
+{
+    var key = array[i];
+    var j = i - 1;
+    while (j >= 0 && array[j] > key)
+    {
+        array[j+1] = array[j];
+        j--;
+    }
+    array[j + 1] = key;
+}
+watch.Stop();
+
+Console.WriteLine("New Array: ");
+foreach (var num in array)
+{
+    Console.Write($"{num} ");
+}
+Console.WriteLine();
+Console.WriteLine($"Took: {watch.ElapsedMilliseconds}ms");
